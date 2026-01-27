cc1f1641a38a10e18e1ab2bac6b3a1c67a14160c
Abdur Rahman <abdurrahmanariyansyah@gmail.com>
2026-01-27 19:45:25 +0700

    Add database connection configuration file
    add file

---
+++ config/database.php
@@ -0,0 +1,15 @@
+<!-- /var/www/html/config/database.php -->
+<?php
+$host = 'localhost';
+$user = 'absensi_user';
+$pass = 'password123';
+$dbname = 'absensi_siswa';
+
+$conn = mysqli_connect($host, $user, $pass, $dbname);
+
+if (!$conn) {
+    die("Koneksi database gagal: " . mysqli_connect_error());
+}
+
+mysqli_set_charset($conn, "utf8");
+?>
